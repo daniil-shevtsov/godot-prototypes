a8687bdaa43f1fbdb1b8840b93b46be58ee91d2e
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-06-18 18:07:42 +0400

    it works

--- Game.cs
+++ Game.cs
@@ -4,11 +4,17 @@ using System;
 public partial class Game : Node2D
 {
 
+	private RigidBody2D Player;
+	private RigidBody2D UpperArm;
+	private RigidBody2D LowerArm;
 	private RigidBody2D Gun;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Player = (RigidBody2D)FindChild("Player");
+		UpperArm = (RigidBody2D)FindChild("UpperArm");
+		LowerArm = (RigidBody2D)FindChild("LowerArm");
 		Gun = (RigidBody2D)FindChild("Gun");
 	}
 
@@ -21,10 +27,18 @@ public partial class Game : Node2D
 	{
 		var sensitivity = 100;
 		var speed = 50;
+		var force = 5;
 		if (@event is InputEventMouseMotion eventMouseMotion)
 		{
-			Gun.LookAt(GetGlobalMousePosition());
-			Gun.MoveAndCollide(eventMouseMotion.Relative);
+			//Gun.LookAt(GetGlobalMousePosition());
+			// Gun.ApplyCentralImpulse(eventMouseMotion.Relative * force);
+			// Gun.MoveAndCollide(eventMouseMotion.Relative);
+			var armLength = 450f - 30f;
+			var newDistance = Player.GlobalPosition.DistanceTo(Gun.GlobalPosition + eventMouseMotion.Relative);
+			if (newDistance <= armLength)
+			{
+				Gun.MoveAndCollide(eventMouseMotion.Relative);
+			}
 		}
 	}
 }
