1aad4986924451910ddf64e59a8ed8530c7720f9
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-06-18 18:25:49 +0400

    spawn bullets

--- Game.cs
+++ Game.cs
@@ -9,6 +9,8 @@ public partial class Game : Node2D
 	private RigidBody2D LowerArm;
 	private RigidBody2D Gun;
 
+	private PackedScene BulletScene;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -16,11 +18,21 @@ public partial class Game : Node2D
 		UpperArm = (RigidBody2D)FindChild("UpperArm");
 		LowerArm = (RigidBody2D)FindChild("LowerArm");
 		Gun = (RigidBody2D)FindChild("Gun");
+
+		BulletScene = GD.Load<PackedScene>("res://bullet.tscn");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (Input.IsMouseButtonPressed(MouseButton.Left))
+		{
+			SpawnBullet();
+		}
 	}
 
 	public override void _Input(InputEvent @event)
@@ -40,5 +52,18 @@ public partial class Game : Node2D
 				Gun.MoveAndCollide(eventMouseMotion.Relative);
 			}
 		}
+	}
+
+	private void SpawnBullet()
+	{
+
+		var bulletSpeed = 10f;
+		var bullet = (RigidBody2D)BulletScene.Instantiate();
+		bullet.GlobalPosition = Gun.GlobalPosition;// + new Vector2(300f, 0f);
+		bullet.RotationDegrees = 90f;
+		bullet.LinearVelocity = new Vector2(bulletSpeed, 10f);
+		AddChild(bullet);
+
+		GD.Print($"Spawn bullet at {bullet.GlobalPosition}");
 	}
 }
