6739be5085dab9eae33a1fd1837d5f1d18e7f395
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-06-19 15:29:00 +0400

    trying

--- Game.cs
+++ Game.cs
@@ -78,19 +78,19 @@ public partial class Game : Node2D
     private void MoveGun(InputEventMouseMotion eventMouseMotion)
     {
         var armLength = 450f - 30f;
-        var newDistance = Player.GlobalPosition.DistanceTo(
-            Gun.GlobalPosition + eventMouseMotion.Relative
-        );
+        var sensitivity = 0.75f;
+        var distanceChange = eventMouseMotion.Relative * 0.5f;
+        var newDistance = Player.GlobalPosition.DistanceTo(Gun.GlobalPosition + distanceChange);
         if (newDistance <= armLength)
         {
-            var collision = Gun.MoveAndCollide(eventMouseMotion.Relative);
+            var collision = Gun.MoveAndCollide(distanceChange);
             if (collision != null)
             {
                 HandleCollision(collision);
             }
             else
             {
-                Gun.GlobalPosition += eventMouseMotion.Relative;
+                Gun.GlobalPosition += distanceChange;
             }
         }
     }
@@ -101,15 +101,15 @@ public partial class Game : Node2D
         var collider = collision.GetCollider() as RigidBody2D;
         var mass = collider.Mass;
         Vector2 normal = collision.GetNormal();
-        float forceFactor = 50.0f;
+        float forceFactor = 10.0f;
 
-        Vector2 impulse = remainder.Length() * -normal * forceFactor * mass;
+        Vector2 impulse = remainder.Length() * -normal * forceFactor;
 
         GD.Print(
-            $"Parsed collider {collider} remainder {remainder} normal {normal} impulse {impulse}"
+            $"Parsed collider {collider} remainder {remainder} normal {normal} impulse {impulse} mass {mass}"
         );
 
-        collider.ApplyCentralImpulse(impulse);
+        collider.ApplyCentralImpulse(impulse / mass);
 
         Gun.GlobalPosition += remainder;
     }
