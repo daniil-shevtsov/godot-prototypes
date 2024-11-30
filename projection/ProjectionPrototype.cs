c5acec9feb370cc7b1c3e76b014e7d03b6357673
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-12-01 00:46:35 +0400

    move logic

--- projection/ProjectionPrototype.cs
+++ projection/ProjectionPrototype.cs
@@ -12,6 +12,7 @@ public partial class ProjectionPrototype : Node3D
 	private PanelContainer panelContainer;
 	private Label label;
 	private AnimatedSprite2D shrek;
+	private ProjectionPlayer player;
 
 	private Vector2 originalSize = Vector2.Zero;
 	private Vector2 totalSizeChange = Vector2.Zero;
@@ -20,6 +21,12 @@ public partial class ProjectionPrototype : Node3D
 	private float zoomMultiplier = 1f;
 
 	private bool isStretching = true;
+
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -30,6 +37,7 @@ public partial class ProjectionPrototype : Node3D
 		panelContainer = (PanelContainer)FindChild("PanelContainer");
 		shrek = (AnimatedSprite2D)FindChild("AnimatedSprite2D");
 		label = (Label)FindChild("Label");
+		player = (ProjectionPlayer)FindChild("Player");
 
 		RenderingServer.ViewportSetClearMode(subViewport.GetViewportRid(), RenderingServer.ViewportClearMode.Never);
 		var activeMaterial = projectionQuad.MaterialOverride;
@@ -160,6 +168,43 @@ public partial class ProjectionPrototype : Node3D
 		// {
 		// 	subViewport.Size = subViewport.Size / zoomMultiplier;
 		// }
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = player.Velocity;
+
+		// Add the gravity.
+		if (!player.IsOnFloor())
+		{
+			velocity.Y -= gravity * (float)delta;
+		}
+
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && player.IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(player.Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(player.Velocity.Z, 0, Speed);
+		}
+
+		player.Velocity = velocity;
+		player.MoveAndSlide();
 	}
 
 	private String VerticesString(Vector3[] vertices)
