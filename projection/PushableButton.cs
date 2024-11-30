9c13cf8cffdcba652f69c75c2a6271688d053d71
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-12-01 02:55:26 +0400

    isenabled works

--- projection/PushableButton.cs
+++ projection/PushableButton.cs
@@ -6,14 +6,21 @@ public partial class PushableButton : StaticBody3D
 	[Export]
 	public float resistance = 0f;
 
+	[Export]
+	public float enabledThreshold = 0.20f;
+
 	public SliderJoint3D SliderJoint;
 	public RigidBody3D ButtonBody;
+	public MeshInstance3D ButtonMesh;
+
+	public bool IsEnabled = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		SliderJoint = (SliderJoint3D)FindChild("ButtonSlider");
 		ButtonBody = (RigidBody3D)FindChild("ButtonBody");
+		ButtonMesh = (MeshInstance3D)FindChild("ButtonMesh");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,5 +31,25 @@ public partial class PushableButton : StaticBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		ButtonBody.ApplyCentralImpulse(Transform.Basis.Y * resistance * (float)delta);
+
+		var range = SliderJoint.GetParam(SliderJoint3D.Param.LinearLimitUpper) * 2;
+		var differenceFromCenter = SliderJoint.GlobalPosition - ButtonBody.GlobalPosition;
+		var oldIsEnabled = IsEnabled;
+		IsEnabled = differenceFromCenter.Y > enabledThreshold;
+
+		GD.Print($"{range} {differenceFromCenter} {IsEnabled}");
+		if (oldIsEnabled != IsEnabled)
+		{
+			StandardMaterial3D material = new StandardMaterial3D();
+			if (IsEnabled)
+			{
+				material.AlbedoColor = Color.FromHtml("#00FF00");
+			}
+			else
+			{
+				material.AlbedoColor = Color.FromHtml("#FF0000");
+			}
+			ButtonMesh.MaterialOverride = material;
+		}
 	}
 }
