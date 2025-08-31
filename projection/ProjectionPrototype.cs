2afc4e81bf2816af6906133c2a3c7523dc3a9063
Daniil Shevtsov <shevtsov200@gmail.com>
2025-08-31 21:33:06 +0200

    cleanup

--- projection/ProjectionPrototype.cs
+++ projection/ProjectionPrototype.cs
@@ -1,33 +1,23 @@
 using Godot;
 using System;
 using System.Linq;
+using Prototypes.projection.asset.player;
 
 public partial class ProjectionPrototype : Node3D
 {
 	private TvLogic tvLogic= new();
+	private PlayerLogic _playerLogic = new();
 	
 	private Remote remote;
 	
-	private ProjectionPlayer player;
 	private RigidBody3D testPlayer;
 
 	private PushableButton groundButton;
 
 	public Joint3D playerHolderJoint;
-
-	private Vector2 totalSizeChange = Vector2.Zero;
 	
-	private bool isHolding = false;
-
-	[Export] private float speed = 5.0f;
 	[Export] private float mouseSensitivity = 0.01f;
-	[Export] private float jumpForce = 10.0f;
-	[Export] private float gravity = 3.8f;
-
-	[Export] private float pushForce = 500f; //TODO: Calculate from player mass
-
-	private Vector3 velocity = Vector3.Zero;
-	private Vector2 rotation = Vector2.Zero;
+	
 	private float targetPitch = 0f;
 	private float targetYaw = 0f;
 
@@ -37,7 +27,7 @@ public partial class ProjectionPrototype : Node3D
 	public override void _Ready()
 	{
 		tvLogic.init(this);
-		initPlayer();
+		_playerLogic.Init(this);
 		initProps();
 	}
 
@@ -83,72 +73,12 @@ public partial class ProjectionPrototype : Node3D
 			}
 		}
 
-		Vector3 direction = Vector3.Zero;
-
-		if (Input.IsActionPressed("forward"))
-			direction -= player.Transform.Basis.Z;
-		if (Input.IsActionPressed("backwards"))
-			direction += player.Transform.Basis.Z;
-		if (Input.IsActionPressed("left"))
-			direction -= player.Transform.Basis.X;
-		if (Input.IsActionPressed("right"))
-			direction += player.Transform.Basis.X;
-
-		direction = direction.Normalized();
-		velocity.X = direction.X * speed;
-		velocity.Z = direction.Z * speed;
-
-		if (player.IsOnFloor() && Input.IsActionJustPressed("jump"))
-		{
-			velocity.Y = jumpForce;
-		}
-		else
-		{
-			velocity.Y += -gravity * (float)delta;
-		}
-		player.Velocity = velocity;
-
-		player.MoveAndSlide();
-
-		var collision = player.GetLastSlideCollision();
-		if (collision != null)
-		{
-			var collider = collision.GetCollider();
-			var collisionPosition = collision.GetPosition();
-
-			if (collider is RigidBody3D)
-			{
-				var body = collider as RigidBody3D;
-				var pushDirection = -collision.GetNormal();
-				var pushPosition = collisionPosition - body.GlobalPosition;
-				body.ApplyImpulse(pushDirection * pushForce * (float)delta, pushPosition);
-			}
-		}
+		var input = Input.GetVector("left", "right", "backwards", "forward");
+		_playerLogic.HandleInput(input, (float)delta);
 
 		tvLogic.Button(groundButton.IsEnabled);
 
-		if (isHolding)
-		{
-			remote.GlobalPosition = player.handMarker.GlobalPosition;
-			GD.Print($"TELEPORT REMOTE");
-			PhysicsServer3D.BodySetState(
-				remote.GetRid(),
-				PhysicsServer3D.BodyState.Transform,
-				Transform3D.Identity.Translated(player.handMarker.GlobalPosition)
-			);
-		}
-
-// 		var currentYaw = testPlayer.Rotation.Y;
-// 		var yawError   = Mathf.AngleDifference(currentYaw, targetYaw);
-//
-// 		float stiffness = 30f;
-// 		float damping   = 5f;
-//
-// 		float torqueY = (yawError * stiffness) - (testPlayer.AngularVelocity.Y * damping);
-// 		testPlayer.ApplyTorque(new Vector3(0, torqueY, 0));
-//
-// // Apply pitch directly to camera (not physics, just rotation)
-// 		testPlayer.GetNode<Camera3D>("Camera3D").Rotation = new Vector3(targetPitch, 0, 0);
+		_playerLogic.PhysicsProcess((float)delta);
 	}
 
 
@@ -156,21 +86,7 @@ public partial class ProjectionPrototype : Node3D
 	{
 		if (@event is InputEventMouseMotion mouseMotion)
 		{
-			rotation.Y -= mouseMotion.Relative.X * mouseSensitivity;
-			rotation.X -= mouseMotion.Relative.Y * mouseSensitivity;
-			rotation.X = Mathf.Clamp(rotation.X, -Mathf.Pi / 2, Mathf.Pi / 2);
-
-			// player.RotationDegrees = new Vector3(0, Mathf.RadToDeg(rotation.Y), 0);
-			var newCameraRotation = new Vector3(Mathf.RadToDeg(rotation.X), 0, 0);
-			// player.tpsCamera.RotationDegrees = newCameraRotation;
-			// player.fpsCamera.RotationDegrees = newCameraRotation;
-			// var tpsCamera = testPlayer.GetNode<Camera3D>("Camera3D");
-			// if (GetViewport().GetCamera3D() != tpsCamera)
-			// {
-			// 	player.tpsCamera.Current = false;
-			// 	player.fpsCamera.Current = false;
-			// 	tpsCamera.Current = true;
-			// }
+			_playerLogic.HandleMouseInput(mouseMotion, mouseSensitivity);
 			
 			targetYaw   -= mouseMotion.Relative.X * mouseSensitivity;
 			targetPitch -= mouseMotion.Relative.Y * mouseSensitivity;
@@ -180,32 +96,12 @@ public partial class ProjectionPrototype : Node3D
 		if (Input.IsActionJustReleased("use"))
 		{
 			tvLogic.ToggleStretching();
-
-			GD.Print("USE PRESSED");
-			isHolding = !isHolding;
-			// if (isHolding)
-			// {
-			// 	DropRemote();
-			// }
-			// else
-			// {
-			// 	GrabRemote();
-			// }
-
+			_playerLogic.HandleAction(PlayerAction.Use);
 		}
 
 		if (Input.IsActionJustReleased("toggle_camera"))
 		{
-			if (GetViewport().GetCamera3D() == player.tpsCamera)
-			{
-				player.tpsCamera.Current = false;
-				player.fpsCamera.Current = true;
-			}
-			else
-			{
-				player.tpsCamera.Current = true;
-				player.fpsCamera.Current = false;
-			}
+			_playerLogic.HandleAction(PlayerAction.ToggleCamera);
 		}
 	}
 
@@ -214,33 +110,12 @@ public partial class ProjectionPrototype : Node3D
 		return $"{vertices.Length} {String.Join(", ", vertices.Select(vertex => $"{vertex}"))}";
 	}
 
-	public void DropRemote()
-	{
-		isHolding = false;
-	}
-
-	public void GrabRemote()
-	{
-		// remote.GlobalPosition = player.handMarker.GlobalPosition;
-		isHolding = true;
-	}
+	
 
 	private void initProps()
 	{
 		groundButton = (PushableButton)FindChild("GroundButton");
 		remote = (Remote)FindChild("Remote");
-	}
-
-	private void initPlayer()
-	{
-		player = (ProjectionPlayer)FindChild("Player");
-		player.fpsCamera.Current = false;
-		player.tpsCamera.Current = true;
-		
-		//var kek = player.playerHolderJoint.GlobalPosition;
-		//kek.Y = kek.Y + remote.collisionShape.Size.Y / 2f;
-		//remote.GlobalPosition = kek;
-		//player.playerHolderJoint.NodeB = remote.GetPath();
 	}
 
 	private void initTestPlayer()
---
+++ projection/asset/player/PlayerLogic.cs
@@ -0,0 +1,167 @@
+using Godot;
+
+namespace Prototypes.projection.asset.player;
+
+public enum PlayerAction
+{
+    Use,
+    ToggleCamera
+}
+public class PlayerLogic
+{
+    private Node _root;
+    private ProjectionPlayer player;
+    private Remote remote;
+
+
+    private Vector3 velocity = Vector3.Zero;
+    private Vector2 rotation = Vector2.Zero;
+
+    public void Init(Node root)
+    {
+        _root = root;
+        player = root.FindChild("Player") as ProjectionPlayer;
+        initPlayer();
+        remote = root.FindChild("Remote") as Remote;
+    }
+    
+    private void initPlayer()
+    {
+        player.fpsCamera.Current = false;
+        player.tpsCamera.Current = true;
+		
+        //var kek = player.playerHolderJoint.GlobalPosition;
+        //kek.Y = kek.Y + remote.collisionShape.Size.Y / 2f;
+        //remote.GlobalPosition = kek;
+        //player.playerHolderJoint.NodeB = remote.GetPath();
+    }
+    
+    public void PhysicsProcess(float delta)
+    {
+        if (isHolding)
+        {
+            remote.GlobalPosition = player.handMarker.GlobalPosition;
+            GD.Print($"TELEPORT REMOTE");
+            PhysicsServer3D.BodySetState(
+                remote.GetRid(),
+                PhysicsServer3D.BodyState.Transform,
+                Transform3D.Identity.Translated(player.handMarker.GlobalPosition)
+            );
+        }
+        
+        player.Velocity = velocity;
+
+        player.MoveAndSlide();
+
+        var collision = player.GetLastSlideCollision();
+        if (collision != null)
+        {
+            var collider = collision.GetCollider();
+            var collisionPosition = collision.GetPosition();
+
+            if (collider is RigidBody3D body)
+            {
+                var pushDirection = -collision.GetNormal();
+                var pushPosition = collisionPosition - body.GlobalPosition;
+
+                body.ApplyImpulse(pushDirection * pushForce, pushPosition); // remove * delta
+            }
+        }
+    }
+
+    public void HandleMouseInput(InputEventMouseMotion mouseMotion, float mouseSensitivity)
+    {
+        rotation.Y -= mouseMotion.Relative.X * mouseSensitivity;
+        rotation.X -= mouseMotion.Relative.Y * mouseSensitivity;
+        rotation.X = Mathf.Clamp(rotation.X, -Mathf.Pi / 2, Mathf.Pi / 2);
+
+        // player.RotationDegrees = new Vector3(0, Mathf.RadToDeg(rotation.Y), 0);
+        var newCameraRotation = new Vector3(Mathf.RadToDeg(rotation.X), 0, 0);
+        // player.tpsCamera.RotationDegrees = newCameraRotation;
+        // player.fpsCamera.RotationDegrees = newCameraRotation;
+        // var tpsCamera = testPlayer.GetNode<Camera3D>("Camera3D");
+        // if (GetViewport().GetCamera3D() != tpsCamera)
+        // {
+        // 	player.tpsCamera.Current = false;
+        // 	player.fpsCamera.Current = false;
+        // 	tpsCamera.Current = true;
+        // }
+    }
+
+    public void HandleAction(PlayerAction action)
+    {
+        if (action == PlayerAction.Use)
+        {
+            isHolding = !isHolding;
+            // if (isHolding)
+            // {
+            // 	DropRemote();
+            // }
+            // else
+            // {
+            // 	GrabRemote();
+            // }
+        } else if (action == PlayerAction.ToggleCamera)
+        {
+            if (_root.GetViewport().GetCamera3D() == player.tpsCamera)
+            {
+                player.tpsCamera.Current = false;
+                player.fpsCamera.Current = true;
+            }
+            else
+            {
+                player.tpsCamera.Current = true;
+                player.fpsCamera.Current = false;
+            }
+        }
+    }
+    
+    public void HandleInput(Vector2 input, float delta)
+    {
+        Vector3 direction = Vector3.Zero;
+
+        if (input.Y > 0)
+            direction -= player.Transform.Basis.Z;
+        if (input.Y < 0)
+            direction += player.Transform.Basis.Z;
+        if (input.X < 0)
+            direction -= player.Transform.Basis.X;
+        if (input.X > 0)
+            direction += player.Transform.Basis.X;
+
+        direction = direction.Normalized();
+        
+        velocity.X = direction.X * speed;
+        velocity.Z = direction.Z * speed;
+
+        if (player.IsOnFloor() && Input.IsActionJustPressed("jump"))
+        {
+            velocity.Y = jumpForce;
+        }
+        else
+        {
+            velocity.Y += -gravity * (float)delta;
+        }
+    }
+    
+    private void DropRemote()
+    {
+        isHolding = false;
+    }
+
+    private void GrabRemote()
+    {
+        // remote.GlobalPosition = player.handMarker.GlobalPosition;
+        isHolding = true;
+    }
+    
+    [Export] private float speed = 5.0f;
+    [Export] private float jumpForce = 3.0f;
+    [Export] private float gravity = 3.8f;
+    [Export] private float pushForce = 1f; //TODO: Calculate from player mass
+
+    
+    private bool isHolding = false;
+
+
+}
